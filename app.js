7914d5d9f19613bfeee4e11bae59258bc4250734
JuanV <juanvme123@gmail.com>
2015-03-26 01:32:04 -0700

    Several bug fixes to prevent breaking match states
    Fixed the following bugs:
    - after a match is completed, tapping the losing player’s incrementer
    div still incremented the non-winning player’s score
    - after a match is completed, tapping the winning player’s incrementer
    div triggered the post-game recap div

--- app.js
+++ app.js
@@ -1,79 +1,70 @@
-angular.module('pingPong', ['firebase'])
+angular.module('pingPong', ['firebase', 'ngTouch'])
     .controller('mainCtrl', ['$scope', '$firebase', '$window', '$timeout', '$http', '$firebaseArray', '$firebaseObject', function($scope, $firebase, $window, $timeout, $http, $firebaseArray, $firebaseObject) {
-        var players = new Firebase("https://pingpongscore.firebaseio.com/players");
-        var players = $firebaseObject(players);
+        // 3-way binding of a single "match" object from Firebase   
+        var match = new Firebase("https://pingpongscore.firebaseio.com/match");
+        var match = $firebaseObject(match);
+        match.$bindTo($scope, "match");
+        $scope.match = match;
 
-        // For three-way data bindings, bind it to the scope instead
-        players.$bindTo($scope, "players");
-        $scope.players = players;
-
-        $scope.matchInSession = true;
-        $scope.mostRecentVictory = {
-            player: 'Player A'
-        }
+        // Resets the state of the match to allow new players to play
+        $scope.newGame = function() {
+            $scope.match = {
+                matchInSession: true,
+                awaitingPlayers: false,
+                combinedScore: 0,
+                players: [{
+                    name: "Player A",
+                    isCurrentServer: true,
+                    score: 0
+                }, {
+                    name: "Player B",
+                    isCurrentServer: false,
+                    score: 0
+                }]
+            };
+        };
+        // Adds a point to the corresponding player
         $scope.addNewPoint = function(player) {
-            if (player.score >= 20) {
-                // player.score++;
+            if (player.score >= 20 && $scope.match.matchInSession === true) {
                 player.score = 21;
                 player.isWinner = true;
-                $scope.mostRecentVictory.player = player.name;
+                $scope.match.mostRecentVictor = player.name;
                 $scope.gameOver();
-
-            } else if ($scope.matchInSession === true) {
+            } else if ($scope.match.matchInSession === true) {
                 player.score++;
-                $scope.combinedScore++;
-                if ($scope.combinedScore % 5 === 0) {
-                    if ($scope.players[0].isCurrentServer === true) {
-                        $scope.players[0].isCurrentServer = false;
-                        $scope.players[1].isCurrentServer = true;
+                $scope.match.combinedScore++;
+                // Switches the server every 5 rounds
+                // Todo: when a player reaches 20 points, the other player will serve until they tie or the game ends 
+                if ($scope.match.combinedScore % 5 === 0) {
+                    if ($scope.match.players[0].isCurrentServer === true) {
+                        $scope.match.players[0].isCurrentServer = false;
+                        $scope.match.players[1].isCurrentServer = true;
                     } else {
-                        $scope.players[0].isCurrentServer = true;
-                        $scope.players[1].isCurrentServer = false;
+                        $scope.match.players[0].isCurrentServer = true;
+                        $scope.match.players[1].isCurrentServer = false;
                     }
                 };
             }
         };
+
+        // Show the victor for five seconds before inviting new players to start a match        
         $scope.gameOver = function() {
-            $scope.matchInSession = false;
+            $scope.match.matchInSession = false;
+            if($scope.match.awaitingPlayers === false){
+            $scope.match.showRecap = true;
+        }
+            $timeout(function() {
+                            $scope.match.matchInSession === false;
 
+                $scope.match.showRecap = false;
 
+                $scope.match.awaitingPlayers = true;
+            }, 5000);
         };
-        $scope.newGame = function() {
-            $scope.players = [{
-                name: 'Player A',
-                isCurrentServer: true,
-                score: 0
-            }, {
-                name: 'Player B',
-                isCurrentServer: false,
-                score: 0
-            }];
-            $scope.matchInSession = true;
-            $scope.combinedScore = 0;
-        };
-        $scope.combinedScore = 0;
-        // $scope.combinedScore = function() {
-        //     var count = 0;
-        //     angular.forEach($scope.players, function(player) {
-        //         count += player.score;
-        //     });
-        //     return count;
-        // };
-        $scope.archive = function() {
-            var oldTodos = $scope.todos;
-            $scope.todos = [];
-            angular.forEach(oldTodos, function(todo) {
-                if (!todo.done) $scope.todos.push(todo);
-            });
-        };
-    }])
-    .directive('stRatio', function() {
-        return {
-            link: function(scope, element, attr) {
-                var ratio = +(attr.stRatio);
+    }]);
 
-                element.css('width', ratio + '%');
+        // Main goal for v2: refactor controller logic into services+factories d to allow instances of matches to be created, recorded and recalled. 
 
-            }
-        };
-    });
+        // Possible cool features for future versions: in-office leaderboard, winning streaks, memory of past
+        // matches between two players (ie: it could teasingly remind a player how often they've lost against 
+        // a certain player, and likewise congratulate frequent champions on their skills
